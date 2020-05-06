191d7dd0a1ec1a34c1e2d392d25b8270cbf344bc
flippedbit <straughan.michael@gmail.com>
2020-05-06 01:24:28 -0400

    adding return type to getID, returns string and error

--- plex.go
+++ plex.go
@@ -12,7 +12,7 @@ import (
 const plexURL = "http://192.168.1.88:32400/"
 const plexToken = "?X-Plex-Token=FFH5BBKsCW3iSgnDxynW"
 
-type MediaContainer struct {
+type MovieLibrary struct {
 	XMLName xml.Name `xml:"MediaContainer"`
 	Movies  []Movie  `xml:"Video"`
 }
@@ -44,28 +44,28 @@ type Actor struct {
 }
 
 // com.plexapp.agents.imdb://tt0458525?lang=en
-func (m *Movie) getID() string {
+func (m *Movie) getID() (string, error) {
 	id := strings.Split(m.GUID, "//")
 	id = strings.Split(id[1], "?")
 	if strings.Contains(id[0], "tt") {
-		return id[0]
+		return id[0], nil
 	} else {
-		return ""
+		return "", fmt.Errorf("Could not get ID")
 	}
 }
 
 func main() {
 	libraryURL := "library/sections/1/all"
 
-	var movies MediaContainer
+	var movies MovieLibrary
 
 	url := plexURL + libraryURL + plexToken
-	println(url)
 	r, err := http.Get(url)
 	if err != nil {
 		println(err)
 		os.Exit(1)
 	}
+	defer r.Body.Close()
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -75,6 +75,8 @@ func main() {
 
 	xml.Unmarshal(body, &movies)
 	for _, m := range movies.Movies {
-		fmt.Println("Movie: " + m.Title + " ID: " + m.getID())
+		if id, err := m.getID(); err == nil {
+			fmt.Println("Movie: " + m.Title + " ID: " + id)
+		}
 	}
 }
